c8927520b2b2ebf8ee0da04ccbc0b47c1e20059f
Yana Sanzhieva <yanasanzhieva@gmail.com>
2022-02-13 21:48:38 +0300

    ci: break

--- src/main/kotlin/WallService.kt
+++ src/main/kotlin/WallService.kt
@@ -1,3 +1,27 @@
 object WallService {
-    var posts = emptyArray<Post>()
+    private var posts = emptyArray<Post>()
+    private var uniqueIds = emptyArray<Int>()
+
+    fun add(post: Post): Post {
+        if (uniqueIds.contains(post.id)) {
+            println("Ошибка. Такой пост уже существует")
+        } else {
+            uniqueIds += post.id
+            posts += post
+        }
+        return posts.last()
+    }
+
+    fun update(post: Post): Boolean {
+        if (uniqueIds.contains(post.id)) {
+            for ((index, item) in posts.withIndex()) {
+                if (item.id == post.id) {
+                    val newCopy = post.copy(ownerId = item.ownerId, date = item.date)
+                    posts[index] = newCopy
+                }
+            }
+            return true
+        }
+        return false
+    }
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,6 @@
 fun main() {
-
+    WallService.add(Post(1, 12, 34, 18, text = "Статья о собаках"))
+    WallService.update(Post(1, 56, 48, 12, 1640544454654, "Статья о собаках дополненная"))
+    for(post in WallService.posts){
+        println(post)}
 }
