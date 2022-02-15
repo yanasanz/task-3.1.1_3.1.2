80b5037cfc8c8b2cbec5d0f03493e86958a2afa1
Yana Sanzhieva <yanasanzhieva@gmail.com>
2022-02-15 19:01:25 +0300

    WallService.kt fixed

--- src/main/kotlin/WallService.kt
+++ src/main/kotlin/WallService.kt
@@ -3,18 +3,16 @@ class WallService {
     private var uniqueIds = emptyArray<Int>()
 
     fun add(post: Post): Post {
-        if (uniqueIds.contains(post.id)) {
-            println("Ошибка. Такой пост уже существует")
-        } else {
-            val newId = if (uniqueIds.isEmpty()) 1 else uniqueIds.last() + 1
-            uniqueIds += newId
-            posts += post.copy(newId)
-        }
+        val newId = if (uniqueIds.isEmpty()) 1 else uniqueIds.last() + 1
+        uniqueIds += newId
+        posts += post.copy(id = newId)
         return posts.last()
     }
 
     fun update(post: Post): Boolean {
-        if (uniqueIds.contains(post.id)) {
+        if (!uniqueIds.contains(post.id)) {
+            return false
+        } else {
             for ((index, item) in posts.withIndex()) {
                 if (item.id == post.id) {
                     val newCopy = post.copy(ownerId = item.ownerId, date = item.date)
@@ -23,6 +21,5 @@ class WallService {
             }
             return true
         }
-        return false
     }
 }
